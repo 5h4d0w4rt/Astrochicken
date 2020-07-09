4ba97aeb908f4bbc3517c44e07420567db1efeb4
5h4d0w4rt <5h4d0w4rt@users.noreply.github.com>
2020-07-09 22:42:28 +0300

    init bunch of stuff

---
+++ bash/.bashrc
@@ -0,0 +1,2 @@
+# some weird workaround with askpass bugs
+sudo ln -s /usr/local/bin/ssh-askpass /usr/X11R6/bin/ssh-askpass
---
+++ osx/install.sh
@@ -0,0 +1,14 @@
+set -euo pipefail
+
+_brew_installed() {
+    command -v brew >/dev/null 2>&1
+}
+# install brew
+if _brew_installed; then
+    echo "brew is installed"
+else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+# install all the packages
+for x in $(cat ./brew_packages.txt); do brew install $x; done
